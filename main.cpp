142a3a7f0ff38889973d6d48f735ab210620957c
Adesh CANNOU <adeshca156@gmail.com>
2023-03-17 19:01:51 +0100

    moteur

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include "mbed.h"
 #include "Serial.h"
+#include "StepperMotor.h"
+
+// Configure les broches pour le pilote A4988
+DigitalOut dirPin(p5);
+DigitalOut stepPin(p6);
+
+// Configure la constante pour le nombre de pas par tour du moteur
+const int stepsPerRevolution = 200;
 
 Serial bt(p28, p27); // définir une connexion série pour le module Bluetooth HC-05
 DigitalOut led1(LED1); // définir une sortie numérique pour la LED1
@@ -8,23 +16,43 @@ DigitalOut electromagnet(p8);
 
 int main() {
     while (1) {
+        //electromagnet = 1;
         if (bt.readable()) { // Vérifier si des données sont disponibles
             char c = bt.getc(); // Lire le caractère reçu
             pc.putc(c);
-            if (c == 'H') { // Si le caractère reçu est 'H' pour "haut"
-                
-            } else if (c == 'B') { // Si le caractère reçu est 'B' pour "bas"
-                
-            } else if (c == 'G') { // Si le caractère reçu est 'G' pour "gauche"
-                
-            } else if (c == 'D') { // Si le caractère reçu est 'D' pour "droite"
-                
-            } else if (c == 'O') { // Si le caractère reçu est 'O'
-                led1 = 1;
-                electromagnet = 1;
-            } else if (c == 'F') { // Si le caractère reçu est 'F'
-                led1 = 0;
-                electromagnet = 0;
+            switch(c) {
+                case 'H':
+                    // code pour le caractère 'H'
+                    break;
+                case 'B':
+                    // code pour le caractère 'B'
+                    break;
+                case 'G':
+                    // code pour le caractère 'G'
+                    led1 = 0;
+                    break;
+                case 'D':
+                    // code pour le caractère 'D'
+                    dirPin = 1;
+                    led1 = 1;
+                    for(int i=0; i<stepsPerRevolution; i++) {
+                        stepPin = 1;
+                        wait_us(2000);
+                        stepPin = 0;
+                        wait_us(2000);
+                    }
+                    break;
+                case 'O':
+                    // code pour le caractère 'O'
+                    electromagnet = 1;
+                    break;
+                case 'F':
+                    // code pour le caractère 'F'
+                    electromagnet = 0;
+                    break;
+                default:
+                    // code pour un caractère inconnu
+                    break;
             }
         }
     }
